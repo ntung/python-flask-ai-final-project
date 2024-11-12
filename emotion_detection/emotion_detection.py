5c2ae55b4d9219b73587ac85bd8f9118a50c4a9b
Tung Nguyen <nvntung@gmail.com>
2024-11-12 12:33:10 -0500

    Task 3: Format the output of the application

--- emotion_detection/emotion_detection.py
+++ emotion_detection/emotion_detection.py
@@ -8,6 +8,18 @@ def emotion_detector(text_to_analyse):
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL, headers=headers, json=input_json)
     # response = requests.post(URL, headers=headers, data=json.dumps(input_json))
-    return response.text
-
+    print(response.text)
+    data_json = json.loads(response.text)
+    # Predicted Emotion
+    pe = data_json["emotionPredictions"][0]["emotion"]
+    returned_data = { 
+        "anger": pe["anger"], 
+        "disgust": pe["disgust"], 
+        "fear": pe["fear"], 
+        "joy": pe["joy"], 
+        "sadness": pe["sadness"]
+    }
+    dominant = max(returned_data, key=returned_data.get)
+    returned_data["dominant_emotion"] = dominant
+    return returned_data
     
