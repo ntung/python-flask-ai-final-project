3ae1831b96108693caf22415d21692d80e4ffda2
Nguyen Tung <nvntung@gmail.com>
2024-11-13 06:35:06 -0500

    Task 8: Run static code analysis

--- server.py
+++ server.py
@@ -1,22 +1,36 @@
+"""
+This is the main file where we define all routes and 
+principal logical for the application.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-
+"""
+Define a route to detect emotion basing on a given statement
+"""
 @app.route("/emotionDetector")
 def detect_emotion():
+    """
+    input: a string indicating a statement
+    output: a statement 
+            either trying again or the actually analysed result
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     dominant_emotion = emotion_detector(text_to_analyze)
-
+    message = ""
     if dominant_emotion["dominant_emotion"] is None:
-        return "Invalid text! Please try again."
+        message = "Invalid text! Please try again."
     else:
-        return "The given text has been identified as {}.".format(dominant_emotion)
+        message = "The given text has been identified as " + dominant_emotion
+
+    return message
 
 
 @app.route("/")
 def render_index_page():
+    """ An only action: rendering the index.html page """
     return render_template('index.html')
 
 
